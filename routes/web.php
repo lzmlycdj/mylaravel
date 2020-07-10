dbfb8b6572cb2337aef885f7b8dd24403985109d
lzm2013lzm <lzm2013lzm@gmail.com>
2020-07-10 15:25:43 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('hello',
+
+function(){
+    echo 'xxxxxx';
+}
+
+
+);
+
+Route::match(['get','post'],'hello',
+function(){
+    return 'hello laravle mathc';
+}
+);
+
+/* Route::any(
+    ['get', 'post'],
+    'hello',
+    function () {
+        return 'hello laravle mathc';
+    }
+); */
+
+
+Route::any(
+    
+    'hello/{id}',
+    function ($id) {
+       echo $id;
+    }
+);
+
+
+
+
+
+
